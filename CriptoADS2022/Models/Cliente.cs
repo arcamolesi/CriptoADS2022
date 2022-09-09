df9cc76fd8970d9573b85c7e1863bc123c696e3f
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-09-08 22:45:34 -0300

    Add project files.

---
+++ CriptoADS2022/Models/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CriptoADS2022.Models
+{
+    public enum Estado {RS, SC, PR, SP, RJ, ES, MG, MS, MT, GO, TO};
+
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        [Required(ErrorMessage ="Campo nome é obrigatório")]
+        [Display(Name ="Nome: ")]
+        public string nome { get; set; }
+
+        [StringLength(25)]
+        [Required(ErrorMessage ="Campo cidade é obrigatório...")]
+        [Display(Name ="Cidade: ")]
+        public string cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo Estado é obrigatório...")]
+        [Display(Name = "Estado: ")]
+        public Estado estado { get; set; }
+        
+        [Display(Name = "Idade: ")]
+        [Range(18, 100, ErrorMessage = "Idade entre 18..100")]
+        public int idade { get; set; }
+
+
+
+    }
+}
---
+++ CriptoADS2022/Models/Moeda.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CriptoADS2022.Models
+{
+    [Table("Moedas")]
+    public class Moeda
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="Id: ")]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        [Required(ErrorMessage ="campo descrição é obrigatório...")]
+        [Display(Name = "Descrição: ")]
+        public string descricao { get; set; }
+
+        [Display(Name = "Compra: ")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public float compra { get; set; }
+
+        [Display(Name = "Venda")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public float venda { get; set; }
+    }
+}
