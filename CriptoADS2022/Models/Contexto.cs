47e9d3c214f7a9e5e74ceda08cec0d1fda3a3e88
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-09-16 19:30:27 -0300

    Atualização com Contexto e AppSettings

--- CriptoADS2022/Models/Contexto.cs
+++ CriptoADS2022/Models/Contexto.cs
@@ -1,6 +1,21 @@
-﻿namespace CriptoADS2022.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CriptoADS2022.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options):
+                                                base (options)
+        {
+           
+        }
+
+        public DbSet<Cliente> clientes { get; set; }
+        public DbSet<Moeda> moedas { get; set; }
+        public DbSet<Conta> contas { get; set; }
+        public DbSet<Transacao> transacoes { get; set; }
+
+
+
     }
 }
