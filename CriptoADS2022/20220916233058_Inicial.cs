a376a3fd630d536170c44d3a531fd9ff859e2007
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-09-22 22:37:48 -0300

    Atualização do projeto - Pagina Layout.cshtml

---
+++ CriptoADS2022/20220916233058_Inicial.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CriptoADS2022.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    cidade = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    estado = table.Column<int>(type: "int", nullable: false),
+                    idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moedas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descricao = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false),
+                    compra = table.Column<float>(type: "real", nullable: false),
+                    venda = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moedas", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conta",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    clienteid = table.Column<int>(type: "int", nullable: true),
+                    moedaid = table.Column<int>(type: "int", nullable: true),
+                    quantidade = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conta", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Conta_Clientes_clienteid",
+                        column: x => x.clienteid,
+                        principalTable: "Clientes",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Conta_Moedas_moedaid",
+                        column: x => x.moedaid,
+                        principalTable: "Moedas",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    contaid = table.Column<int>(type: "int", nullable: false),
+                    data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    quantidade = table.Column<float>(type: "real", nullable: false),
+                    valor = table.Column<float>(type: "real", nullable: false),
+                    operacao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Conta_contaid",
+                        column: x => x.contaid,
+                        principalTable: "Conta",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conta_clienteid",
+                table: "Conta",
+                column: "clienteid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conta_moedaid",
+                table: "Conta",
+                column: "moedaid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_contaid",
+                table: "Transacoes",
+                column: "contaid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Conta");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Moedas");
+        }
+    }
+}
---
+++ CriptoADS2022/20220916233620_AlteraNomeCliente.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CriptoADS2022.Migrations
+{
+    public partial class AlteraNomeCliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "nome",
+                table: "Clientes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "nome",
+                table: "Clientes",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
